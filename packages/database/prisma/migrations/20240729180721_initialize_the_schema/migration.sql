063931cbed026a7966153cac02a4e6dba23a1bbf
Pranit Patil <91155068+PranitPatil03@users.noreply.github.com>
2024-07-29 23:44:28 +0530

    Successfully setup the Supabase Database with Prisma

---
+++ packages/database/prisma/migrations/20240729180721_initialize_the_schema/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "username" TEXT,
+    "gh_username" TEXT,
+    "email" TEXT,
+    "emailVerified" TIMESTAMP(3),
+    "image" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
